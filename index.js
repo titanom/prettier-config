7851b394d210b229ff1fb059c3caa9f88ea348da
Jannis Morgenstern <jannis.morgenstern@titanom.com>
2022-05-19 00:24:37 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const config = {
+  printWidth: 100,
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: true,
+  jsxSingleQuote: false,
+  quoteProps: 'as-needed',
+  trailingComma: 'all',
+  bracketSpacing: true,
+  bracketSameLine: false,
+  arrowParens: 'always',
+  requirePragma: false,
+  insertPragma: false,
+  proseWrap: 'preserve',
+  htmlWhitespaceSensitivity: 'strict',
+  endOfLine: 'lf',
+  embeddedLanguageFormatting: 'off',
+};
+
+module.exports = config;
